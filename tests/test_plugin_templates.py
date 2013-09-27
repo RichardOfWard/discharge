26d3225f88196e4efb668fb8a60f04d04cc8adff
Richard Ward <richard@richard.ward.name>
2013-09-27 12:19:42 +0100

    test brevity

--- tests/test_plugin_templates.py
+++ tests/test_plugin_templates.py
@@ -1,27 +1,25 @@
 from .base import Test
 from discharge.plugins.templates import TemplatesPlugin
 
+
 class TestTemplatesPlugin(Test):
 
     site_dir = 'test_templates_site'
 
-    def test_non_template(self):
+    def setup(self):
+        super(TestTemplatesPlugin, self).setup()
         templates_plugin = TemplatesPlugin()
         self.site.register_plugin(templates_plugin)
         self.builder.build()
+
+    def test_non_template(self):
         with open(self.build_path + '/test.txt') as f:
             assert f.read().strip() == '{{"test"}}'
 
     def test_template(self):
-        templates_plugin = TemplatesPlugin()
-        self.site.register_plugin(templates_plugin)
-        self.builder.build()
         with open(self.build_path + '/test.html') as f:
             assert f.read().strip() == 'test'
 
     def test_var_file(self):
-        templates_plugin = TemplatesPlugin()
-        self.site.register_plugin(templates_plugin)
-        self.builder.build()
         with open(self.build_path + '/test_var_file.html') as f:
             assert f.read().strip() == 'test_var_file.html'
