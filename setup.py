a4ccf6aa40b0a0119f7b94d9f7e099f79162c352
Richard Ward <richard@richard.ward.name>
2013-09-30 17:36:46 +0100

    oops, use correct file name

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Richard Ward',
     author_email='richard@richard.ward.name',
     description='A static site generator',
-    long_description=open('README.txt').read(),
+    long_description=open('README.rst').read(),
     packages=find_packages(exclude=['tests']),
     scripts=['scripts/discharge'],
     install_requires=[
