cd162e28baf7b910fb1a1e574835df75c4704d3f
Richard Ward <richard@richard.ward.name>
2013-09-30 17:33:54 +0100

    use description from readme

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     version=discharge.version,
     author='Richard Ward',
     author_email='richard@richard.ward.name',
-    description='A modular static site generator',
+    description='A static site generator',
+    long_description=open('README.txt').read(),
     packages=find_packages(exclude=['tests']),
     scripts=['scripts/discharge'],
     install_requires=[
